5ec676596ad8d2ffeaa5474dca43107dfc54c2dd
OxygenXXX <oxyyygen.developer@gmail.com>
2022-08-13 22:19:07 +0300

    Added defines for LCD parameters

--- home_station.ino
+++ home_station.ino
@@ -1,6 +1,21 @@
 
 #include "LiquidCrystal.h"
 
+#define LCD_PIN_REGSELECT 8 
+#define LCD_PIN_ENABLED 9
+
+#define LCD_DISPLAY_WIDTH 16
+#define LCD_DISPLAY_HEIGHT 2
+
+#define LCD_PIN_DATA1 4
+#define LCD_PIN_DATA2 5
+#define LCD_PIN_DATA3 6
+#define LCD_PIN_DATA4 7
+
+#define LCD_PIN_BACKLIGHT 10
+
+#define LCD_PIN_BUTTON A0
+
 void setup(void)
 {
 
